a26209f273809779d87ff8980631e1d3325479b9
p <progix.21@gmail.com>
2018-08-24 20:55:11 +0530

    Test Cases Upgrade

--- src/main/java/com/example/demo/controllers/SampleController.java
+++ src/main/java/com/example/demo/controllers/SampleController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SampleController.class);
+	
 	@GetMapping("/hello")
 	public ResponseEntity<String> sayHello(@RequestParam("name") String name ) {
+		LOGGER.info("Request Parametere is {}", name);
 		return new ResponseEntity<String>("Hello, "+ name, HttpStatus.OK);
 	}
 	
