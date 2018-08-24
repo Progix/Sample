cdeb5b8b6f7cd5241c49ce2ba2ae5f92fc48813c
p <progix.21@gmail.com>
2018-08-24 20:48:49 +0530

    Test Cases Upgrade

---
+++ src/test/java/com/example/demo/controllers/SampleControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void testSayHello() throws Exception {
+		mvc.perform(get("/hello?name=Priyesh").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string("Hello, Priyesh"));
+	}
+
+}
